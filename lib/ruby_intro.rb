f1616b64802986a412fc17874f30f79ce2b28644
EC2 Default User <ec2-user@ip-172-31-26-236.us-east-2.compute.internal>
2023-02-08 18:28:19 +0000

    First que

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,101 @@
-# When done, submit this entire file to the autograder.
-
-# Part 1
-
 def sum arr
-  # YOUR CODE HERE
+  
+  s = 0
+  if arr.empty?
+    return 0
+  else
+    for i in 0..arr.length-1
+      s += arr[i]
+    end
+  return s
+  end
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
-end
+# def max_2_sum arr
+#   if arr.length==0
+#     return 0;
+#   elsif arr.length==1
+#     return arr[0]
+#   else
+#     a = arr.sort
+#     return a[-1]+a[-2]
+    
+#   end
+  
+# end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
-end
 
-# Part 2
+# def sum_to_n? arr, n
+#   hash = Hash.new(0)
+#   arr.each do |val| if hash.key? val 
+# return true 
+# else
+# hash[n-val] = val 
+# end 
+#   end
+# return false
+# end
 
-def hello(name)
-  # YOUR CODE HERE
-end
+# # Part 2
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
-end
+# def hello(name)
+# 	"Hello, #{name}"
+# end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
-end
 
-# Part 3
 
-class BookInStock
-# YOUR CODE HERE
-end
+# def starts_with_consonant? s
+#   if s.empty? 
+#     return false
+#   elsif (s[0]=~/^[aeiou]/||s[0,1]=~/^[AEIOU]/) 
+#     return false
+#   elsif (s[0]=~/[a-zA-Z]/)
+#     return false
+#   else
+#     return true
+#   end
+# end
+
+# def binary_multiple_of_4? s
+#   return false if s == "0"
+#   return s.to_i % 4 == 0
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+# # Part 3
+
+# class BookInStock
+
+#   attr_accessor:isn, :price
+
+
+#   def initialize isbn, price
+
+#   raise ArgumentError if isbn.empty? || price <= 0
+
+#   @isbn = isbn
+
+#   @price = price
+
+#   end
+
+# def price_as_string
+
+
+#   format("$%.2f", @price)
+
+
+# end
+
+
+# end
